b74686020b4face5bf8d381dd20ae2c83874fb40
fsz0826 <513302151@qq.com>
2024-12-13 17:31:33 +0800

    使用form组件

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,22 +1,42 @@
 <script setup lang="ts">
+import { reactive, ref } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus'
 
-import { ref } from 'vue'
+interface Form {
+  account: string
+  password: string
+}
 
-const account = ref('')
-const password = ref('')
+const form = reactive<Form>({
+  account: '',
+  password: '',
+})
 
-function handleLogin() {
-  if (account.value === '' || password.value === '') {
-    alert('请输入账号和密码');
-    return;
-  }
+//表单校验
+const ruleFormRef = ref<FormInstance>()
+const rules = reactive<FormRules<Form>>({
+  account: [{ message: '请输入账号', trigger: 'blur' }],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 6, message: '密码不能少于6位', trigger: 'blur' },
+  ],
+})
 
+function handleLogin(formEl: FormInstance | undefined) {
+  if (!formEl) return
+  formEl.validate((valid, fields) => {
+    console.log(valid, fields)
+    if (valid) {
+      console.log('账号:', form.account)
+      console.log('密码:', form.password)
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
   // 这里可以添加实际的登录逻辑，比如调用 API
-  console.log('账号:', account.value);
-  console.log('密码:', password.value);
+
 
   // 模拟登录成功
-  alert('登录成功')
 }
 </script>
 
@@ -24,26 +44,33 @@ function handleLogin() {
   <div class="main">
     <h1>账号登陆</h1>
     <p>Account Login</p>
-    <form action="" class="form">
-      <label class="account-number">
-        <svg
-          t="1655889286604"
-          class="icon"
-          viewBox="0 0 1024 1024"
-          xmlns="http://www.w3.org/2000/svg"
-          p-id="1495"
-          width="24"
-          height="24"
-        >
-          <path
-            d="M515.131392 563.202048c155.492352 0 281.544704-126.075904 281.544704-281.544704 0-155.492352-126.053376-281.544704-281.544704-281.544704-155.497472 0-281.547776 126.052352-281.547776 281.544704C233.58464 437.126144 359.634944 563.202048 515.131392 563.202048zM643.106816 359.1424c0 0-6.398976 101.680128-131.175424 101.680128-124.777472 0-124.777472-101.680128-124.777472-101.680128L643.106816 359.1424zM515.131392 614.391808c-332.112896 0-409.522176 170.066944-409.522176 409.496576l819.04128 0C924.650496 783.058944 848.638976 614.391808 515.131392 614.391808z"
-            p-id="1496"
-            fill="#00cccc"
-          ></path>
-        </svg>
-        <input v-model="account" type="text" placeholder="请输入账号" />
-      </label>
-      <label class="pwd">
+    <el-form ref="ruleFormRef" :rules="rules" :model="form" class="form">
+      <el-form-item class="account-number" prop="account">
+        <template v-slot:label>
+          <svg
+            t="1655889286604"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="1495"
+            width="24"
+            height="24"
+          >
+            <path
+              d="M515.131392 563.202048c155.492352 0 281.544704-126.075904 281.544704-281.544704 0-155.492352-126.053376-281.544704-281.544704-281.544704-155.497472 0-281.547776 126.052352-281.547776 281.544704C233.58464 437.126144 359.634944 563.202048 515.131392 563.202048zM643.106816 359.1424c0 0-6.398976 101.680128-131.175424 101.680128-124.777472 0-124.777472-101.680128-124.777472-101.680128L643.106816 359.1424zM515.131392 614.391808c-332.112896 0-409.522176 170.066944-409.522176 409.496576l819.04128 0C924.650496 783.058944 848.638976 614.391808 515.131392 614.391808z"
+              p-id="1496"
+              fill="#00cccc"
+            ></path>
+          </svg>
+        </template>
+        <el-input
+          v-model="form.account"
+          style="width: 362px; height: 40px"
+          type="text"
+          placeholder="请输入账号"
+        />
+      </el-form-item>
+      <el-form-item class="pwd" prop="password">
         <svg
           t="1655889313328"
           class="icon"
@@ -59,17 +86,22 @@ function handleLogin() {
             fill="#00cccc"
           ></path>
         </svg>
-        <input v-model="password" type="password" placeholder="请输入密码" />
-      </label>
-
-    </form>
-    <div class="sign-bar">
+        <el-input
+          v-model="form.password"
+          style="width: 362px; height: 40px"
+          type="password"
+          placeholder="请输入密码"
+          show-password
+        />
+      </el-form-item>
+    </el-form>
+    <div class="login-bar">
       <a href="">忘记密码</a>
-      <a href="#" class="signin">去注册</a>
+      <a href="#" class="login">去注册</a>
     </div>
-    <div class="btn-bar">
-      <button @click="handleLogin" type="submit">登陆</button>
-    </div>
+    <el-form-item class="btn-bar">
+      <el-button type="primary" @click="handleLogin(ruleFormRef)">登陆</el-button>
+    </el-form-item>
   </div>
 </template>
 
@@ -86,7 +118,7 @@ select {
 .main {
   width: 100%;
   height: 100vh;
-  background: url("@/assets/images/signInBackground.jpg") center no-repeat;
+  background: url('@/assets/images/signInBackground.jpg') center no-repeat;
   background-size: cover;
   margin: 0 auto;
   display: flex;
@@ -122,12 +154,12 @@ select {
       }
     }
 
-    .icon {
-      margin-right: 16px;
-    }
+    //.icon {
+    //  margin-right: 16px;
+    //}
   }
 
-  .sign-bar {
+  .login-bar {
     margin-top: 18px;
     display: flex;
     justify-content: space-between;
@@ -138,7 +170,7 @@ select {
       font-size: 12px;
       color: #000;
 
-      &.signin {
+      &.login {
         color: #0cc;
       }
     }
